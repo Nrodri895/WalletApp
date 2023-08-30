b2fb289d43e2863e410815597f1fffa9fdc827d9
Carlos Agudelo <charlidagudelo@gmail.com>
2023-08-30 18:04:23 -0500

    adelanto de clase 30-08-2023

---
+++ app/src/main/java/com/NicolasRodriguez/walletapp/NewAccountActivity2.java
@@ -0,0 +1,13 @@
+package com.NicolasRodriguez.walletapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewAccountActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account2);
+    }
+}
