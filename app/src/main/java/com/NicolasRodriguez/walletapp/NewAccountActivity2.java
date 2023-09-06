db2af2490d3d3bf85384f67bdabe3634b923d2f6
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2023-09-06 17:59:45 -0500

    adelanto clase 06-09-23

--- app/src/main/java/com/NicolasRodriguez/walletapp/NewAccountActivity2.java
+++ app/src/main/java/com/NicolasRodriguez/walletapp/NewAccountActivity2.java
@@ -1,13 +1,33 @@
 package com.NicolasRodriguez.walletapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NewAccountActivity2 extends AppCompatActivity {
+
+    private ImageView ivBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account2);
+
+        ivBack = findViewById(R.id.iv_back);
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent =new Intent( NewAccountActivity2.this,MainActivity.class );
+                startActivity(myIntent);
+
+            }
+        });
+
+
     }
 }
